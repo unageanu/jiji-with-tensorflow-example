e0332823859d89723559bef6db3bc4f7b9d6ffb7
unageanu <masaya.yamauchi@gmail.com>
2016-08-09 11:11:21 +0900

    Fix bug.

--- scripts/server.py
+++ scripts/server.py
@@ -5,24 +5,6 @@ from flask import Flask, jsonify, request
 from trade_results_loader import *
 from model import *
 
-# x = tf.placeholder("float", [None, 784])
-# sess = tf.Session()
-#
-# with tf.variable_scope("simple"):
-#     y1, variables = model.simple(x)
-# saver = tf.train.Saver(variables)
-# saver.restore(sess, "mnist/data/simple.ckpt")
-# def simple(input):
-#     return sess.run(y1, feed_dict={x: input}).flatten().tolist()
-#
-# with tf.variable_scope("convolutional"):
-#     keep_prob = tf.placeholder("float")
-#     y2, variables = model.convolutional(x, keep_prob)
-# saver = tf.train.Saver(variables)
-# saver.restore(sess, "mnist/data/convolutional.ckpt")
-# def convolutional(input):
-#     return sess.run(y2, feed_dict={x: input, keep_prob: 1.0}).flatten().tolist()
-
 loader = TradeResultsLoader()
 
 sell_estimator = Estimator("sell")
@@ -33,21 +15,26 @@ buy_estimator = Estimator("buy")
 buy_estimator.__enter__()
 buy_estimator.restore("../data/buy.ckpt")
 
+sell_data = loader.retrieve_trade_data("sell").train_data()
+buy_data  = loader.retrieve_trade_data("buy").train_data()
+
+
 def read_data(body):
-    p body
     sell_or_buy = body["sell_or_buy"]
     del body["sell_or_buy"]
-    base = loader.retrieve_trade_data(sell_or_buy)
-    return pd.DataFrame(body).append(base)
+    data = pd.DataFrame({k: [v] for k, v in body.items()}).append(
+        sell_data if sell_or_buy == "sell" else buy_data)
+    return (data, sell_or_buy)
 
 # webapp
 app = Flask(__name__)
 
 @app.route('/api/estimator', methods=['POST'])
 def estimate():
-    data = read_data(request.json)
+    data, sell_or_buy = read_data(request.json)
+    estimator = sell_estimator if sell_or_buy == "sell" else buy_estimator
     results = estimator.estimate(TradeResults.normalize(data))
-    return jsonify(results=results[0])
+    return jsonify(result=float(results[0][0]))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
