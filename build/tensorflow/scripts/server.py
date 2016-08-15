1597292903bf1da89992bae0333287569d962613
unageanu <masaya.yamauchi@gmail.com>
2016-08-15 12:24:14 +0900

    Add comment.

--- build/tensorflow/scripts/server.py
+++ build/tensorflow/scripts/server.py
@@ -18,7 +18,9 @@ app = Flask(__name__)
 
 @app.route('/api/estimator', methods=['POST'])
 def estimate():
+    # 値の正規化のため、リクエストボディで渡された指標データと訓練で使用したデータを統合。
     data = pd.DataFrame({k: [v] for k, v in request.json.items()}).append(trade_data)
+    # 正規化したデータを渡して、損益を予測
     results = estimator.estimate(TradeResults(data).all_data().iloc[[0]])
     return jsonify(result=("up" if results[0] == 0 else "down"))
 
